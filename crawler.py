ca424531033556deff3c2a558835a67a51005e54
Soohyungbaik <baik1204@nexon.co.kr>
2025-07-04 14:54:51 +0900

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -7,7 +7,7 @@ from email.mime.text import MIMEText
 import shutil
 
 # 오늘 날짜 (테스트용으로 수동 고정 가능)
-today = '2025-06-27'
+today = datetime.today().strftime('%Y-%m-%d')
 
 # 뉴스 소스 URL
 source_url = f"https://soohyungbaik.github.io/my-news-daily/dailynews/{today}.html"
